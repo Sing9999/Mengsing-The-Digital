5998767883ad29ba019345c4cdb8967fd7e350f3
Sing9999 <215302194+Sing9999@users.noreply.github.com>
2025-06-10 22:57:27 +0700

    Update bot.py

--- myproject/bot.py
+++ myproject/bot.py
@@ -1,1 +1,11 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+BOT_TOKEN = "ใส่โทเคนของบอทพี่ตรงนี้"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("สวัสดี! บอทของพี่มารายงานตัวแล้วครับ!")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
